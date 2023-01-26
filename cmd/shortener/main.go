a816cfbf45d198ce1e54e5ffbc68e941bd25b8c4
Nikolay Eroshenkov <nikolay.eroshenkov@mail.ru>
2023-01-26 10:11:56 +0300

    Basic functionality works.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var urlInputForm = `
+<html>
+    <head>
+    <title></title>
+    </head>
+    <body>
+        <form method="post">
+            <label>Enter URL to shorten: </label><input type="text" name="url">
+            <input type="submit" value="OK">
+        </form>
+    </body>
+</html>
+`
+
+var urlStore []string
+
+func Shortener(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		if len(r.URL.Query()) == 0 {
+			fmt.Fprint(w, urlInputForm)
+			return
+		}
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			http.Error(w, "Short URL identificator is missing", http.StatusBadRequest)
+			return
+		}
+		id64, err := strconv.ParseUint(id, 10, 64)
+		if err != nil {
+			http.Error(w, "Short URL identificator must be an unsigned integer", http.StatusBadRequest)
+			return
+		}
+		if id64 >= uint64(len(urlStore)) {
+			http.Error(w, "Short URL does not exist", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprint(w, urlStore[id64])
+	case http.MethodPost:
+		url := r.FormValue("url")
+		// Then check if url is URL indeed
+		urlStore = append(urlStore, url)
+		// fmt.Fprint(w, url)
+	default:
+		http.Error(w, "Only GET or POST requests are allowed", http.StatusMethodNotAllowed)
+	}
+}
 
 func main() {
-	fmt.Println("ok3")
+	urlStore = make([]string, 0)
+	http.HandleFunc("/", Shortener)
+	http.ListenAndServe("localhost:8080", nil)
+	// Consider to use log.Fatal(http.ListenAndServe("localhost:8080", nil)) instead
 }
