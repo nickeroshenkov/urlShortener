c059a199dad0390eb78eefd163913b16b4721ec5
Nikolay Eroshenkov <nikolay.eroshenkov@mail.ru>
2023-02-09 20:22:12 +0300

    ok

--- internal/app/handlers/handlers_test.go
+++ internal/app/handlers/handlers_test.go
@@ -141,7 +141,7 @@ func (store *urlStoreMock) Get (id int) (string, error) {
 }
 
 func TestSetRoute(t *testing.T) {
-	for _, tt := range tests {
+	for _, tt := range tests { 
 		t.Run(tt.name, func(t *testing.T) {
 			store := urlStoreMock { urls: tt.i.URLStore }
 			router := chi.NewRouter()
